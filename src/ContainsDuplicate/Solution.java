ad965e2bb65c75f0e8a0df6c10195aed8b5eb3d8
sandman6094 <wangxiaohang2000@gmail.com>
2021-12-26 00:39:28 -0800

    Adding ContainsDuplicate

--- src/ContainsDuplicate/Solution.java
+++ src/ContainsDuplicate/Solution.java
@@ -1,2 +1,21 @@
-package ContainsDuplicate;public class Solution {
+package ContainsDuplicate;
+
+import java.util.HashMap;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> list = new HashMap<>();
+
+        for (int i : nums) {
+            if (list.containsKey(i)) {
+                return true;
+            }
+
+            else {
+                list.put(i, true);
+            }
+        }
+
+        return false;
+    }
 }
