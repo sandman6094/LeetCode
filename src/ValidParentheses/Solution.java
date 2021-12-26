85249a0065578b50c976141f048b20fd08b43dec
sandman6094 <wangxiaohang2000@gmail.com>
2021-12-25 20:00:04 -0800

    Solved question 20 valid parentheses

--- src/ValidParentheses/Solution.java
+++ src/ValidParentheses/Solution.java
@@ -1,4 +1,45 @@
 package ValidParentheses;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Deque<Character> stc = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '[' || c == '{' || c == '(') {
+                stc.push(c);
+                continue;
+            }
+
+            if (stc.isEmpty()) {
+                return false;
+            }
+
+            switch (c) {
+                case ']':
+                    if (stc.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stc.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ')':
+                    if (stc.pop() != '(') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return (stc.isEmpty());
+    }
 }
